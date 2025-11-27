2ebae408d1895e474034b16b64343fe1fc87ac4a
Hugo Fang <hugo2fang@gmail.com>
2025-11-26 21:11:59 -0500

    refactor

--- src/command_handlers.py
+++ src/command_handlers.py
@@ -1,29 +1,47 @@
-import configparser
-import os
 import asyncio
+import configparser
+import json
+import os
+from collections import defaultdict
+from pathlib import Path
+
 import discord
-from github_integration import volumes, set_volumes_changed
+from discord import DMChannel, TextChannel
+
 from audio_handler import play_audio, set_stop_playing
-from constants import CURRENT_DIR, AUDIO_NAMES, AUDIO_LIST
+from constants import (
+    AUDIO_LIST,
+    AUDIO_NAMES,
+    CURRENT_DIR,
+    DEFAULT_VOLUME,
+    VOLUMES_PATH,
+)
+from github_integration import volumes
 
 config = configparser.ConfigParser()
-CONFIG_PATH = os.path.join(CURRENT_DIR, '..', 'variables.ini')
+CONFIG_PATH = os.path.join(CURRENT_DIR, "..", "variables.ini")
 config.read(CONFIG_PATH)
-USER_IDS = {key: int(value) for key, value in config['USER_IDS'].items()}
-CHANNEL_IDS = {key: int(value) for key, value in config['CHANNEL_IDS'].items()}
-channel_name = config['SETTINGS']['channel_name']
+USER_IDS = {key: int(value) for key, value in config["USER_IDS"].items()}
+CHANNEL_IDS = {key: int(value) for key, value in config["CHANNEL_IDS"].items()}
+channel_name = config["SETTINGS"]["channel_name"]
 
 command_lock = asyncio.Lock()
+
+# Initialize audios and volumes
+AUDIO_EXTENSIONS = [".mp3", ".m4a"]
+with open(VOLUMES_PATH, "r") as f:
+    volumes = defaultdict(lambda: DEFAULT_VOLUME, json.load(f))
 
 
 def set_commands(bot):
     @bot.command()
-    async def play(ctx, audio_name=None, channel=None):
+    async def play(ctx, audio_name: str, channel=None):
         async with command_lock:
             try:
                 idx = int(audio_name) - 1
-                audio_name = AUDIO_NAMES[idx]
-            except ValueError:
+                if 0 <= idx < len(AUDIO_NAMES):
+                    audio_name = AUDIO_NAMES[idx]
+            except (ValueError, TypeError):
                 pass
 
             if ctx.author.bot or not audio_name or audio_name not in AUDIO_NAMES:
@@ -32,11 +50,19 @@ def set_commands(bot):
             if ctx.voice_client and ctx.voice_client.is_playing():
                 await asyncio.sleep(1)
 
-            author_voice_channel = ctx.author.voice.channel if ctx.author.voice else None
+            author_voice_channel = (
+                ctx.author.voice.channel if ctx.author.voice else None
+            )
             if channel:
-                voice_channel = discord.utils.get(ctx.guild.voice_channels, name=channel)
+                voice_channel = discord.utils.get(
+                    ctx.guild.voice_channels, name=channel
+                )
             else:
-                voice_channel = ctx.voice_client.channel if ctx.voice_client else author_voice_channel
+                voice_channel = (
+                    ctx.voice_client.channel
+                    if ctx.voice_client
+                    else author_voice_channel
+                )
 
             if voice_channel is None:
                 return
@@ -68,8 +94,12 @@ def set_commands(bot):
             if ctx.voice_client and ctx.voice_client.is_playing():
                 await asyncio.sleep(1)
 
-            author_voice_channel = ctx.author.voice.channel if ctx.author.voice else None
-            voice_channel = ctx.voice_client.channel if ctx.voice_client else author_voice_channel
+            author_voice_channel = (
+                ctx.author.voice.channel if ctx.author.voice else None
+            )
+            voice_channel = (
+                ctx.voice_client.channel if ctx.voice_client else author_voice_channel
+            )
 
             if voice_channel is None:
                 return
@@ -86,7 +116,7 @@ def set_commands(bot):
                 for _ in range(count):
                     keep_playing = await play_audio(bot_voice_client, audio_name)
                     if not keep_playing:
-                        print('Replay stopped')
+                        print("Replay stopped")
                         break
             finally:
                 # Move back to previous channel or disconnect
@@ -120,21 +150,16 @@ def set_commands(bot):
             await voice_channel.connect()
 
     @bot.command()
-    async def vol(ctx, audio, volume: float = None):
+    async def vol(ctx, audio, volume: float | None = None):
         try:
             idx = int(audio) - 1
             audio = AUDIO_NAMES[idx]
         except ValueError:
             pass
-
         if volume is None:
-            if audio not in AUDIO_NAMES:
-                await ctx.reply('Audio not found')
-            else:
-                volume = volumes[audio]
-                await ctx.reply(f'Current volume: {volume}')
+            volume = volumes[audio]
+            await ctx.reply(f"Current volume: {volume}")
         elif 0 <= volume <= 1:
-            set_volumes_changed()
             volumes[audio] = volume
             print(f'"{audio}" now has volume {volume}')
 
@@ -144,13 +169,15 @@ def set_commands(bot):
 
     @bot.command()
     async def help(ctx):
-        await ctx.reply("Commands: play <name/id> (channel), stop_playing, join, leave, audios, vol <name> <volume>")
+        await ctx.reply(
+            "Commands: play <name/id> (channel), stop_playing, join, leave, audios, vol <name> <volume>"
+        )
 
     @bot.command()
     async def leave(ctx):
         # check if the bot is in a voice channel
         if ctx.author.bot or not ctx.voice_client:
-            print('Bot is not currently in a voice channel.')
+            print("Bot is not currently in a voice channel.")
             return
 
         # disconnect the bot from the current voice channel
@@ -172,9 +199,12 @@ def set_commands(bot):
             return
 
         channel = bot.get_channel(CHANNEL_IDS[channel_name])
+        if not isinstance(channel, (TextChannel, DMChannel)):
+            print("Invalid channel")
+            return
         users = None
-        if ',' in msg:
-            msg, users = msg.rsplit(',', 1)
+        if "," in msg:
+            msg, users = msg.rsplit(",", 1)
 
         if not users:
             await channel.send(f"{msg}")
@@ -191,20 +221,22 @@ def set_commands(bot):
         """
         !send_dm msg, user
         """
-        if ',' not in msg:
-            print('No user selected')
+        if "," not in msg:
+            print("No user selected")
             return
-        msg, user = msg.rsplit(',', 1)
+        msg, user = msg.rsplit(",", 1)
         user = user.strip().lower()
         if user not in USER_IDS:
-            print(f'User {user} not found')
+            print(f"User {user} not found")
             return
 
         try:
             user_obj = bot.get_user(USER_IDS[user])
             await user_obj.send(msg)
         except AttributeError as e:
-            print('Likely error: the bot can only send to users that have shared a server with the bot')
+            print(
+                "Likely error: the bot can only send to users that have shared a server with the bot"
+            )
             print(e)
         except Exception as e:
             print(e)
@@ -213,4 +245,4 @@ def set_commands(bot):
     async def setChannel(ctx, new_channel):
         global channel_name
         channel_name = new_channel
-        print(f'Current channel: {channel_name} - {CHANNEL_IDS[channel_name]}')
+        print(f"Current channel: {channel_name} - {CHANNEL_IDS[channel_name]}")
