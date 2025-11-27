2a4db6c4d5c0954ca95885d7faf60c5c5d1e3152
Hugo Fang <hugo2fang@gmail.com>
2025-11-26 21:14:44 -0500

    refactor

--- src/github_integration.py
+++ src/github_integration.py
@@ -1,21 +1,21 @@
 import atexit
 import json
-import os
 import subprocess
 import sys
 from collections import defaultdict
 
-from constants import AUDIO_NAMES
+from constants import AUDIO_NAMES, VOLUMES_PATH
 
 # initialize audio volumes
-VOLUMES_DIR = os.path.join('..', 'volumes.json')
 DEFAULT_VOLUME = 0.4
 volumes = None
 volumes_changed = False
 
+
 def set_volumes_changed():
     global volumes_changed
     volumes_changed = True
+
 
 def fetch_newest_volumes():
     try:
@@ -23,7 +23,9 @@ def fetch_newest_volumes():
         subprocess.run(["git", "fetch"], check=True)
 
         # Checkout only the latest version of volumes.json
-        subprocess.run(["git", "checkout", "origin/main", "--", "../volumes.json"], check=True)
+        subprocess.run(
+            ["git", "checkout", "origin/main", "--", str(VOLUMES_PATH)], check=True
+        )
 
         print("Successfully fetched volumes.json")
 
@@ -34,14 +36,18 @@ def fetch_newest_volumes():
 def initialize_volumes():
     global volumes
     try:
-        with open(VOLUMES_DIR, "r", encoding="utf-8") as f:
+        with open(VOLUMES_PATH, "r", encoding="utf-8") as f:
             volume_file_content = f.read()
-            volumes = defaultdict(lambda: DEFAULT_VOLUME, json.loads(volume_file_content))
+            volumes = defaultdict(
+                lambda: DEFAULT_VOLUME, json.loads(volume_file_content)
+            )
     except FileNotFoundError:
-        print(f"'{VOLUMES_DIR}' not found")
+        print(f"'{VOLUMES_PATH}' not found")
         sys.exit()
     except json.JSONDecodeError:
-        print(f"Error: Failed to parse '{VOLUMES_DIR}'. Ensure it contains valid JSON.")
+        print(
+            f"Error: Failed to parse '{VOLUMES_PATH}'. Ensure it contains valid JSON."
+        )
         sys.exit()
 
 
@@ -49,7 +55,7 @@ def initialize_volumes():
 def update_volumes():
     global volumes_changed
     if not volumes or not volumes_changed:
-        print('Volumes not changed, no need to push to GitHub')
+        print("Volumes not changed, no need to push to GitHub")
         return
 
     # remove unnecessary entries in VOLUMES
@@ -65,9 +71,9 @@ def update_volumes():
         subprocess.run(["git", "add", "volumes.json"], check=True)
         subprocess.run(["git", "commit", "-m", "update volumes.json"], check=True)
         subprocess.run(["git", "push", "origin", "main"], check=True)
-        print('volumes.json pushed to GitHub')
+        print("volumes.json pushed to GitHub")
     except subprocess.CalledProcessError as e:
-        print(f'Failed to push volumes.json: {e}')
+        print(f"Failed to push volumes.json: {e}")
 
 
 # Startup
