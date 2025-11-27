c959779e07a3fce591ae3994a37154e9d3e96018
Hugo Fang <hugo2fang@gmail.com>
2025-11-26 21:35:59 -0500

    refactor

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import command_handlers
 import event_handlers
 from constants import ROOT_DIR
 
-# initialize bot
+# Initialize bot
 dotenv_path = ROOT_DIR / ".env"
 load_dotenv(dotenv_path)
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -16,11 +16,11 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 bot.remove_command("help")  # to define custom help command
 
-# set events and commands
+# Set events and commands
 event_handlers.set_events(bot)
 command_handlers.set_commands(bot)
 
-# run the bot
+# Start bot
 if TOKEN is None:
     raise RuntimeError("DISCORD_TOKEN not found in environment variables")
-bot.run(TOKEN)  # Start the Discord bot
+bot.run(TOKEN)
